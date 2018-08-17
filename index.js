61d0162aaec017e6df9dfbfd700463e081304cfd
Yuri Tomikawa <yuritomikawa@gmail.com>
2018-08-17 13:18:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your solution in this file!
+const driver = {
+  age: 45,
+  name: 'Jane Doe',
+  haircolor: 'purple'
+}
+
+function updateDriverWithKeyAndValue(driver, key, value) {
+  const copy = { ...driver, [key]: value }
+  return copy
+}
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+  driver[key] = value
+  return driver
+}
+function deleteFromDriverByKey (driver, key) {
+  
+}
